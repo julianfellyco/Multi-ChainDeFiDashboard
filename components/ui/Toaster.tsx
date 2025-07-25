e2e9b56b64067a8b9899297bfe43d4ee1e1db5db
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-25 10:23:02 +1000

    Update Toaster.tsx

--- components/ui/Toaster.tsx
+++ components/ui/Toaster.tsx
@@ -1,1 +1,12 @@
+'use client'
 
+import { Toaster as SonnerToaster } from 'sonner'
+import { useTheme } from 'next-themes'
+
+export function Toaster() {
+  const { theme } = useTheme()
+
+  return (
+    
+  )
+}
