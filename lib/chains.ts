f12fc8db8fa59f0c00b77d328adcd33911133f52
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-25 10:36:57 +1000

    Update chains.ts

--- lib/chains.ts
+++ lib/chains.ts
@@ -1,1 +1,28 @@
+// lib/chains.ts
 
+import { Chain } from 'wagmi'
+import { 
+  mainnet, 
+  polygon, 
+  arbitrum, 
+  base 
+} from 'viem/chains'
+
+export const supportedChains: Chain[] = [
+  {
+    ...mainnet,
+    iconUrl: 'https://raw.githubusercontent.com/wagmi-dev/wagmi/main/packages/assets/chains/1.svg',
+  },
+  {
+    ...polygon,
+    iconUrl: 'https://raw.githubusercontent.com/wagmi-dev/wagmi/main/packages/assets/chains/137.svg',
+  },
+  {
+    ...arbitrum,
+    iconUrl: 'https://raw.githubusercontent.com/wagmi-dev/wagmi/main/packages/assets/chains/42161.svg',
+  },
+  {
+    ...base,
+    iconUrl: 'https://raw.githubusercontent.com/wagmi-dev/wagmi/main/packages/assets/chains/8453.svg',
+  },
+]
